65424ea9eb4c38b1fdc2d1109c4d580a4bcb78d3
Si-Hyeak-KANG <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2021-10-11 14:26:38 +0900

    member VO 생성

--- boardWeb/src/user/MemberVO.java
+++ boardWeb/src/user/MemberVO.java
@@ -1,5 +1,79 @@
 package user;
 
+import java.sql.Date;
+
 public class MemberVO {
 
+	private String id;
+	private String pwd;
+	private String name;
+	private String email;
+	private Date joinDate;
+	private String admin;
+	
+	
+	public MemberVO() {
+		System.out.println("vo持失");
+	}
+
+	public MemberVO(String id, String pwd, String name, String email,String admin) {
+		System.out.println("vo持失");
+		this.id = id;
+		this.pwd = pwd;
+		this.name = name;
+		this.email = email;
+		this.admin = admin;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public String getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(String admin) {
+		this.admin = admin;
+	}
+	
+	
+	
+	
 }
